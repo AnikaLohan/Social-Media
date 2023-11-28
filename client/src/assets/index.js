e3de7e7c506058e5bff4c87b1b04b9f6b7334f2b
AnikaLohan <anikalohan@gmail.com>
2023-11-28 21:33:40 +0530

    complete project added to repo

---
+++ client/src/assets/index.js
@@ -0,0 +1,8 @@
+/*import NoProfile from "./userprofile.png";
+import BgImage from "./img.jpeg";
+export { NoProfile, BgImage };
+*/
+// assets/index.js
+import NoProfile from "./userprofile.png";
+import BgImage from "./img.jpeg";
+export { NoProfile, BgImage };
